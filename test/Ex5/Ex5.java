1557c7dac271d0f59650d9130261832c94a82ea7
yudai miyaguni <s19017@std.it-college.ac.jp>
2019-11-30 23:43:48 +0900

    first commit

--- test/Ex5/Ex5.java
+++ test/Ex5/Ex5.java
@@ -1,9 +1,11 @@
 class Ex5{
 
-	public double ex5_1(int [] a){
-		
-		double ret = 0;
-		
-		return ret;
-	}
+    public double ex5_1(int [] a){
+        double sum = 0;
+        for( int i=0; i < a.length; i++ ) {
+            sum += a[i];
+        }
+        double ave = sum / a.length;
+        return ave;
+    }
 }
